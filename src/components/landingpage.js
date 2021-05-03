f168602def808c7160fe804e91204fd7d26b9638
Niraj-Fonseka <fonseka.live@gmail.com>
2021-05-03 01:29:07 -0500

    starting on the initial rewrite

---
+++ src/components/landingpage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './../content.css';
+import niraj from './../niraj.jpeg'
+import { Layout } from 'antd';
+import { Row, Col } from 'antd';
+
+export default function LandingPage(props){
+
+
+
+    return(
+       <div className="mainview">
+        <Row>
+            <Col span={24}>col</Col>
+        </Row>
+        <Row>
+            <Col span={24}>col</Col>
+        </Row>
+       </div>
+    )
+}
