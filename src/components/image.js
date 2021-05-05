7123f85937c2ad56ec6ee8a679773166531b500b
Niraj-Fonseka <fonseka.live@gmail.com>
2021-05-05 02:25:32 -0500

    working on react spring

--- src/components/image.js
+++ src/components/image.js
@@ -1,87 +1,44 @@
-import React, { useRef, useEffect } from 'react'
-import { useSpring, animated, to } from '@react-spring/web'
-import { useGesture } from 'react-use-gesture'
-import imgs from './data'
+import { useRef, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useControls } from "leva";
+import React from 'react'
+import niraj from './../niraj.jpeg'
 
-import styles from './styles.module.css'
 
-const calcX = (y: number, ly: number) => -(y - ly - window.innerHeight / 2) / 20
-const calcY = (x: number, lx: number) => (x - lx - window.innerWidth / 2) / 20
+const calc = (x, y, rect) => [
+  -(y - rect.top - rect.height / 2) / 5,
+  (x - rect.left - rect.width / 2) / 5,
+  1.4
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-const wheel = (y: number) => {
-  const imgHeight = window.innerWidth * 0.3 - 20
-  return `translateY(${-imgHeight * (y < 0 ? 6 : 1) - (y % (imgHeight * 5))}px`
-}
+export default function Image() {
+  const ref = useRef(null);
+  const [xys, set] = useState([0, 0, 1]);
+  const config = useControls({
+    mass: 1,
+    tension: 170,
+    friction: 26,
+    clamp: false,
+    precision: 0.01,
+    velocity: 0,
+    easing: (t) => t
+  });
+  const props = useSpring({ xys, config });
 
-export default function App() {
-  useEffect(() => {
-    const preventDefault = (e: Event) => e.preventDefault()
-    document.addEventListener('gesturestart', preventDefault)
-    document.addEventListener('gesturechange', preventDefault)
-
-    return () => {
-      document.removeEventListener('gesturestart', preventDefault)
-      document.removeEventListener('gesturechange', preventDefault)
-    }
-  }, [])
-
-  const domTarget = useRef(null)
-  const [{ x, y, rotateX, rotateY, rotateZ, zoom, scale }, api] = useSpring(
-    () => ({
-      rotateX: 0,
-      rotateY: 0,
-      rotateZ: 0,
-      scale: 1,
-      zoom: 0,
-      x: 0,
-      y: 0,
-      config: { mass: 5, tension: 350, friction: 40 },
-    })
-  )
-
-  const [{ wheelY }, wheelApi] = useSpring(() => ({ wheelY: 0 }))
-
-  useGesture(
-    {
-      onDrag: ({ active, offset: [x, y] }) =>
-        api.start({ x, y, rotateX: 0, rotateY: 0, scale: active ? 1 : 1.1 }),
-      onPinch: ({ offset: [d, a] }) => api.start({ zoom: d / 200, rotateZ: a }),
-      onMove: ({ xy: [px, py], dragging }) =>
-        !dragging &&
-        api.start({
-          rotateX: calcX(py, y.get()),
-          rotateY: calcY(px, x.get()),
-          scale: 1.1,
-        }),
-      onHover: ({ hovering }) =>
-        !hovering && api.start({ rotateX: 0, rotateY: 0, scale: 1 }),
-      onWheel: ({ event, offset: [, y] }) => {
-        event.preventDefault()
-        wheelApi.set({ wheelY: y })
-      },
-    },
-    { domTarget, eventOptions: { passive: false } }
-  )
   return (
-    <div className={styles.container}>
+    <div  ref={ref}>
       <animated.div
-        ref={domTarget}
-        className={styles.card}
-        style={{
-          transform: 'perspective(600px)',
-          x,
-          y,
-          scale: to([scale, zoom], (s, z) => s + z),
-          rotateX,
-          rotateY,
-          rotateZ,
-        }}>
-        <animated.div style={{ transform: wheelY.to(wheel) }}>
-          {imgs.map((img, i) => (
-            <div key={i} style={{ backgroundImage: `url(${img})` }} />
-          ))}
-        </animated.div>
+        style={{ transform: props.xys.to(trans) }}
+        onMouseLeave={() => set([0, 0, 1])}
+        onMouseMove={(e) => {
+          const rect = ref.current.getBoundingClientRect();
+          set(calc(e.clientX, e.clientY, rect));
+        }}
+       >
+      <img style={{ borderRadius: "5px", marginTop: "100px" , boxShadow:"200px"}} src={niraj} height="325" width="300"  />
       </animated.div>
     </div>
-  )
+  );
 }
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './../content.css';
-import niraj from './../niraj.jpeg'
 import ParticlesBg from 'particles-bg'
 import { Row, Col } from 'antd';
+import Image from './image';
 
 export default function LandingPage(props){
 
@@ -12,8 +12,7 @@ export default function LandingPage(props){
        <div className="mainview">
         <Row>
             <Col span={24}>
-                <img style={{ borderRadius: "5px", marginTop: "100px" , boxShadow:"200px"}} src={niraj} height="325" width="300"  />
-
+                <Image />
             </Col>
         </Row>
         <ParticlesBg type="cobweb" bg={true} color="#874A5B" />
