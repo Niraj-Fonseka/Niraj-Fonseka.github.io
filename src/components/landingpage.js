d1d04e935f6771bcfec1cb627fce1dc07b1faf7b
Niraj-Fonseka <fonseka.live@gmail.com>
2021-05-05 02:04:53 -0500

    getting the particles-bg to work

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './../content.css';
 import niraj from './../niraj.jpeg'
-import { Layout } from 'antd';
+import ParticlesBg from 'particles-bg'
 import { Row, Col } from 'antd';
 
 export default function LandingPage(props){
@@ -11,11 +11,13 @@ export default function LandingPage(props){
     return(
        <div className="mainview">
         <Row>
-            <Col span={24}>col</Col>
+            <Col span={24}>
+                <img style={{ borderRadius: "5px", marginTop: "100px" , boxShadow:"200px"}} src={niraj} height="325" width="300"  />
+
+            </Col>
         </Row>
-        <Row>
-            <Col span={24}>col</Col>
-        </Row>
+        <ParticlesBg type="cobweb" bg={true} color="#874A5B" />
+
        </div>
     )
 }
