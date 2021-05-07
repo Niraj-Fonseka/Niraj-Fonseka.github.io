03a6db37a6868af6b0063fd80e3a118c0caccd9f
Niraj-Fonseka <fonseka.live@gmail.com>
2021-05-06 23:52:03 -0500

    adding prof pic / background / button animations

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './../content.css';
 import ParticlesBg from 'particles-bg'
 import { Row, Col } from 'antd';
-import Image from './image';
+import niraj from './../niraj.jpeg';
+import { Button, Radio } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
 
 export default function LandingPage(props){
 
@@ -12,7 +14,23 @@ export default function LandingPage(props){
        <div className="mainview">
         <Row>
             <Col span={24}>
-                <Image />
+                <img className="profile" src={niraj}/>
+            </Col>
+        </Row>
+        <Row>
+            <Col span={24}>
+                <div className="buttons">
+                    <Button className="button" type="primary"  size="default" />
+                    <Button className="button" type="primary" size="default" />
+                    <Button className="button" type="primary" size="default"/>
+                </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col span={24}>
+                <div className="morebuttons">
+                    <Button className="morebutton" type="primary" size="default" />
+                </div>
             </Col>
         </Row>
         <ParticlesBg type="cobweb" bg={true} color="#874A5B" />
